d10dabd3a8faa807093a2d9217380b679b8d5f97
ashwini reddy <achalla@microsoft.com>
2016-05-30 18:34:08 +0530

    addedProj

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Thread.Sleep(10000);
+        }
+    }
+}
